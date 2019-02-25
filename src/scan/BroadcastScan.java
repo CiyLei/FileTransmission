c1ef2299d634dfeb5460c75a967e2f8deea1f100
cl <903736662@qq.com>
2019-02-25 09:27:49 +0800

    fix: 广播名称解码

--- src/scan/BroadcastScan.java
+++ src/scan/BroadcastScan.java
@@ -143,7 +143,7 @@ public class BroadcastScan implements Scan {
                     if (respone.startsWith(config.broadcastSendTag()) || respone.startsWith(config.broadcastReceiveTag())) {
                         String[] split = respone.split(",");
                         if (split.length == 3) {
-                            String hostName = split[1];
+                            String hostName = new String(Base64.getDecoder().decode(split[1]), "utf-8");
                             Integer port = Integer.parseInt(split[2]);
                             for (Scan.ScanListener listener : listeners) {
 //                              System.out.println(respone);
