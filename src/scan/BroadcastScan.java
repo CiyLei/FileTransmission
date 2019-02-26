6ec53e1fc69024ff68be07666c90a2ff00c1b6d9
CiyLei <903736662@qq.com>
2019-02-26 21:00:26 +0800

    opt: 扫描回调优化

--- src/scan/BroadcastScan.java
+++ src/scan/BroadcastScan.java
@@ -157,11 +157,11 @@ public class BroadcastScan implements Scan {
                             String hostName = config.decodeString(split[1]);
                             Integer port = Integer.parseInt(split[2]);
                             SocketClient client = new SocketClient(datagramPacket.getAddress().getHostAddress(), hostName, port, config);
+                            config.addClient(client);
                             for (Scan.ScanListener listener : listeners) {
 //                              System.out.println(respone);
                                 listener.onGet(client);
                             }
-                            config.addClient(client);
                             // 如果是接受到广播的话，就进行回复,否则的话就是回复广播，不理他
                             if (respone.startsWith(config.broadcastSendTag())) {
                                 reply(datagramPacket.getAddress().getHostAddress());
